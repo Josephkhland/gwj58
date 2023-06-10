ecd897f59aefd8e42bc329ee2b84d1806f325ab7
Josephkhland <Josephkhland@gmail.com>
2023-06-10 23:04:17 +0300

    Added puddles and added some tools to make them snap on the grid whenever it is needed (even through the editor)

---
+++ Scenes/Obstacles/Puddle.gd
@@ -0,0 +1,39 @@
+tool
+extends Position2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var tile_size :int  =32
+export (bool) var grid_snap = true setget set_snap_to_grid
+
+func set_snap_to_grid(value):
+	grid_snap = value
+	if (value):
+		snap_to_grid()
+
+func _init():
+	add_to_group("Obstacles")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	snap_to_grid()
+	#add_to_group("Obstacles")
+	
+
+func snap_to_grid():
+	var x_diff = int(global_position.x) %tile_size
+	var y_diff = int(global_position.y) %tile_size
+	var new_position_no_offset = global_position - Vector2(x_diff, y_diff)
+	var new_position_w_offset = new_position_no_offset + Vector2(tile_size/2, tile_size/2)
+	var new_diff = global_position - new_position_w_offset
+	global_position = new_position_w_offset
+	print(new_diff)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Engine.editor_hint:
+		if grid_snap:
+			snap_to_grid()
+			grid_snap = false
