1075d8e2622a6aa8e710b2e385c53133743fdff5
Josephkhland <Josephkhland@gmail.com>
2023-06-17 15:37:23 +0300

    Added God Icons

--- Scenes/ShrineObject/ShrineObject.gd
+++ Scenes/ShrineObject/ShrineObject.gd
@@ -1,3 +1,4 @@
+tool
 extends Sprite
 
 
@@ -13,9 +14,26 @@ const points_gained_for_correct_flavor = 0
 const points_lost_for_incorrect_flavor =0
 
 export var god_name = "Chizuru"
-export(Texture) var god_image
+export(Texture) var god_image setget set_sprite
+export(Vector2) var god_scale setget set_god_scale
+export(Vector2) var god_position setget set_god_image_position
+
 
 var inventory: InventoryClass
+
+func set_god_image_position(new_position):
+	god_position = new_position
+	$Cloud/God.position = new_position
+
+func set_god_scale(new_scale):
+	god_scale = new_scale
+	$Cloud/God.scale = new_scale
+
+func set_sprite(texture):
+	god_image = texture
+	if god_image != null:
+		$Cloud/God.texture = god_image
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -59,16 +77,17 @@ func get_score(order, tribute_item):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if order == null:
-		yield(get_tree().create_timer(wait_between_orders), "timeout")
-		order = OrderClass.new()
-		$Cloud/God.texture = god_image
-		$Cloud/Ingredient.texture = ItemsDictionary.Dict[order.ingredient.to_lower()].item_icon
-		$Cloud/Ingredient.scale = Vector2(1,1)*2
-		show_cloud()
-	elif order != null and inventory.size() > 0:
-		var score = get_score(order, inventory.inventory[0])
-		print(score)
-		order = null
-		inventory.clear()
-		hide_cloud()
+	if !Engine.editor_hint:
+		if order == null:
+			yield(get_tree().create_timer(wait_between_orders), "timeout")
+			order = OrderClass.new()
+			$Cloud/God.texture = god_image
+			$Cloud/Ingredient.texture = ItemsDictionary.Dict[order.ingredient.to_lower()].item_icon
+			$Cloud/Ingredient.scale = Vector2(1,1)*2
+			show_cloud()
+		elif order != null and inventory.size() > 0:
+			var score = get_score(order, inventory.inventory[0])
+			print(score)
+			order = null
+			inventory.clear()
+			hide_cloud()
